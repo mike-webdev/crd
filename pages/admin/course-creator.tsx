20f41a867e81317cecd698e491664ffee031f520
mike <mike.estonanto18@gmail.com>
2021-08-24 10:53:06 +0800

    CHnaged

--- pages/admin/course-creator.tsx
+++
@@ -1,9 +0,0 @@
-import Card from "../../components/admin/Card";
-
-export default function specializationCreator(){
-    return(
-        <div className="mt-4 mb-4">
-            <Card />
-        </div>
-    )
-}
