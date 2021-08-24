d923c8bf2990859cd22522b7105c361cdbecbc45
mike <mike.estonanto18@gmail.com>
2021-08-24 11:11:06 +0800

    Update FeaturedRealtimeReviews.tsx

--- components/sections/FeaturedRealtimeReviews.tsx
+++ components/sections/FeaturedRealtimeReviews.tsx
@@ -1,70 +1,70 @@
 import StudentReviewsItem from "../items/StudentReviewsItem";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import RealTimeReviews from "../items/RealTimeReviews";
 
 export default function StudentReviewsStudent() {
-    function NextArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-          <div className="control-right" onClick={onClick}>
-            <i className="fas fa-chevron-right"></i>
-          </div>
-        );
-      }
+    // function NextArrow(props) {
+    //     const { className, style, onClick } = props;
+    //     return (
+    //       <div className="control-right" onClick={onClick}>
+    //         <i className="fas fa-chevron-right"></i>
+    //       </div>
+    //     );
+    //   }
     
-      function PrevArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-          <div className="control-left" onClick={onClick}>
-            <i className="fas fa-chevron-left"></i>
-          </div>
-        );
-      }
+    //   function PrevArrow(props) {
+    //     const { className, style, onClick } = props;
+    //     return (
+    //       <div className="control-left" onClick={onClick}>
+    //         <i className="fas fa-chevron-left"></i>
+    //       </div>
+    //     );
+    //   }
     
-      var settings = {
-        centerPadding: 0,
-        dots: false,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        centerMode: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true,
-              centerMode: false,
-              arrows: false,
-            },
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              centerMode: false,
-              arrows: false,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              centerMode: false,
-              dots: true,
-              arrows: false,
-            },
-          },
-        ],
-      };
+    //   var settings = {
+    //     centerPadding: 0,
+    //     dots: false,
+    //     speed: 300,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     nextArrow: <NextArrow />,
+    //     prevArrow: <PrevArrow />,
+    //     centerMode: true,
+    //     responsive: [
+    //       {
+    //         breakpoint: 1024,
+    //         settings: {
+    //           slidesToShow: 2,
+    //           slidesToScroll: 1,
+    //           infinite: true,
+    //           dots: true,
+    //           centerMode: false,
+    //           arrows: false,
+    //         },
+    //       },
+    //       {
+    //         breakpoint: 600,
+    //         settings: {
+    //           slidesToShow: 2,
+    //           slidesToScroll: 1,
+    //           centerMode: false,
+    //           arrows: false,
+    //         },
+    //       },
+    //       {
+    //         breakpoint: 480,
+    //         settings: {
+    //           slidesToShow: 1,
+    //           slidesToScroll: 1,
+    //           centerMode: false,
+    //           dots: true,
+    //           arrows: false,
+    //         },
+    //       },
+    //     ],
+    //   };
     return (
         <section className="student-reviews2 space" id="featured-realtime-reviews">
             <div className="container">
@@ -74,7 +74,7 @@ export default function StudentReviewsStudent() {
                 <div className="row">
                     <div className="col-lg-7">
                         <h3 className="text-center mb-4">Featured Reviews</h3>
-                        <Slider {...settings}>
+                        {/* <Slider {...settings}>
                         <StudentReviewsItem 
                             img="https://images.pexels.com/photos/6238078/pexels-photo-6238078.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
                             name="Name" rating="4.9" course="Mintography" review="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
@@ -91,7 +91,7 @@ export default function StudentReviewsStudent() {
                             img="https://images.pexels.com/photos/3755716/pexels-photo-3755716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
                             name="Name" rating="4.9" course="Mintography" review="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
                 viverra justo eget vestibulum semper. "/>
-                </Slider>
+                </Slider> */}
                     </div>
                     <div className="col-lg-5">
                         <h3 className="text-center mb-4" id="realtimereviews-title">Realtime Reviews</h3>
